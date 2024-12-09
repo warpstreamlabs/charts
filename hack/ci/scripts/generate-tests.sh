82e6c0e407412b2e3a79483ef67f48b4762a8838
Ryan Belgrave <rbelgrave@confluent.io>
2024-12-09 13:27:28 -0600

    need -values

--- hack/ci/scripts/generate-tests.sh
+++ hack/ci/scripts/generate-tests.sh
@@ -9,13 +9,25 @@ config:
     virtualClusterID: "${DefaultVirtualClusterID}"
     region: "us-east-1"
     apiKey: "${DefaultVirtualClusterAgentKeySecret}"
+
+# overriding resources so it fits on a runner
+resources:
+  requests:
+    cpu: 1
+    memory: 4Gi
+    # we do not need the disk space, but Kubernetes will count some logs that it emits
+    # about our containers towards our containers ephemeral usage and if we requested
+    # 0 storage we could end up getting evicted unnecessarily when the node is under disk pressure.
+    ephemeral-storage: "100Mi"
+  limits:
+    memory: 4Gi
 EOM
 set -e
 
-echo "${ci_test_apikey_buildin}" | envsubst > charts/warpstream-agent/ci/apikey-buildin.yaml
+echo "${ci_test_apikey_buildin}" | envsubst > charts/warpstream-agent/ci/apikey-buildin-values.yaml
 
 set +e
-read -r -d '' ci_test_apikey_buildin << EOM
+read -r -d '' ci_test_apikey_external << EOM
 config:
     bucketURL: "mem://mem_bucket"
     virtualClusterID: "${DefaultVirtualClusterID}"
@@ -23,7 +35,19 @@ config:
     apiKeySecretKeyRef:
         name: external-secret
         key: agentkey
+
+# overriding resources so it fits on a runner
+resources:
+  requests:
+    cpu: 1
+    memory: 4Gi
+    # we do not need the disk space, but Kubernetes will count some logs that it emits
+    # about our containers towards our containers ephemeral usage and if we requested
+    # 0 storage we could end up getting evicted unnecessarily when the node is under disk pressure.
+    ephemeral-storage: "100Mi"
+  limits:
+    memory: 4Gi
 EOM
 set -e
 
-echo "${ci_test_apikey_buildin}" | envsubst > charts/warpstream-agent/ci/apikey-external.yaml
+echo "${ci_test_apikey_external}" | envsubst > charts/warpstream-agent/ci/apikey-external-values.yaml
