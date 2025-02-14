eefdac83ca7a60df17c06e562ea5ffe8a1fb1b7a
Ryan Belgrave <rbelgrave@confluent.io>
2025-02-14 15:47:00 -0600

    add support for s3 express

--- hack/ci/scripts/generate-tests.sh
+++ hack/ci/scripts/generate-tests.sh
@@ -2,6 +2,7 @@
 set -e
 set -o pipefail
 
+# Test for setting the apiKey via Helm
 set +e
 read -r -d '' ci_test_apikey_buildin << EOM
 config:
@@ -26,6 +27,7 @@ set -e
 
 echo "${ci_test_apikey_buildin}" | envsubst > charts/warpstream-agent/ci/apikey-buildin-values.yaml
 
+# Test for setting the apiKey via external secret
 set +e
 read -r -d '' ci_test_apikey_external << EOM
 config:
@@ -52,6 +54,7 @@ set -e
 
 echo "${ci_test_apikey_external}" | envsubst > charts/warpstream-agent/ci/apikey-external-values.yaml
 
+# Test for using agent groups
 set +e
 read -r -d '' ci_test_apikey_agent_group << EOM
 config:
@@ -76,3 +79,30 @@ EOM
 set -e
 
 echo "${ci_test_apikey_agent_group}" | envsubst > charts/warpstream-agent/ci/apikey-agent-group-values.yaml
+
+# Test for using s3 express
+set +e
+read -r -d '' ci_test_apikey_s3express << EOM
+config:
+  virtualClusterID: "${DefaultVirtualClusterID}"
+  region: "us-east-1"
+  apiKey: "${DefaultVirtualClusterAgentKeySecret}"
+  s3ExpressEnabled: true
+  ingestionBucketURL: "warpstream_multi://mem://mem_bucket<>mem://mem_bucket<>mem://mem_bucket"
+  compactionBucketURL: "mem://mem_bucket"
+
+# overriding resources so it fits on a runner
+resources:
+  requests:
+    cpu: 1
+    memory: 4Gi
+    # we do not need the disk space, but Kubernetes will count some logs that it emits
+    # about our containers towards our containers ephemeral usage and if we requested
+    # 0 storage we could end up getting evicted unnecessarily when the node is under disk pressure.
+    ephemeral-storage: "100Mi"
+  limits:
+    memory: 4Gi
+EOM
+set -e
+
+echo "${ci_test_apikey_s3express}" | envsubst > charts/warpstream-agent/ci/apikey-agent-group-values.yaml
